64a1746d84e74fc2d3d52241c59818266f189442
dattu145 <sridharreddy145233@gmail.com>
2025-12-30 08:47:47 +0530

    modified files

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
         View::composer('*', function ($view) {
             $view->with('globalSettings', [
                 'logo' => \App\Models\Setting::get('logo'),
+                'logo_url' => \App\Models\Setting::get('logo_url'),
+                'logo_width' => \App\Models\Setting::get('logo_width'),
+                'logo_height' => \App\Models\Setting::get('logo_height'),
+                'whatsapp_number' => \App\Models\Setting::get('whatsapp_number'),
             ]);
             $view->with('globalMenu', \App\Models\MenuItem::ordered()->active()->get());
         });
